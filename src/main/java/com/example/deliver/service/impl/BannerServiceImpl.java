debf8af02ecd575a815e08b6942e26469cd8ab31
inguuu <dlsrb8949@gmail.com>
2019-08-29 18:24:20 +0900

    banner serviceimp#1

--- src/main/java/com/example/deliver/service/impl/BannerServiceImpl.java
+++ src/main/java/com/example/deliver/service/impl/BannerServiceImpl.java
@@ -1,4 +1,23 @@
 package com.example.deliver.service.impl;
 
-public class BannerServiceImpl {
+import com.example.deliver.dto.Banner;
+import com.example.deliver.mapper.BannerMapper;
+import com.example.deliver.model.DefaultRes;
+import com.example.deliver.service.BannerService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BannerServiceImpl implements BannerService {
+    private  final BannerMapper bannerMapper;
+
+    public BannerServiceImpl(BannerMapper bannerMapper) {
+        this.bannerMapper = bannerMapper;
+    }
+
+    @Override
+    public DefaultRes<List<Banner>> getBanner() {
+        List<BannerMapper> bannerList = bannerMapper.getBanner();
+    }
 }
