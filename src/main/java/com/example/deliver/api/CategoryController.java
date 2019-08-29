ddcc989a35e4a3373e7542c7cb5fdee9df918d35
inguuu <dlsrb8949@gmail.com>
2019-08-29 17:40:11 +0900

    cate controller#5

--- src/main/java/com/example/deliver/api/CategoryController.java
+++ src/main/java/com/example/deliver/api/CategoryController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +19,10 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping()
-    public ResponseEntity getCategory(@RequestParam("categoryIdx") int categoryIdx) {
 
+    @GetMapping("/main/category/{categoryIdx}")
+    public ResponseEntity getCategory(@PathVariable("categoryIdx") int categoryIdx) {
+          log.info(categoryIdx+"");
         return new ResponseEntity<>(categoryService.getCategoryList(categoryIdx), HttpStatus.OK);
 
     }
