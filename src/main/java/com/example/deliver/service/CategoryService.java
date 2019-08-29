07cba63cbad2a70333f7eea52d0878a169cac36b
inguuu <dlsrb8949@gmail.com>
2019-08-29 17:03:46 +0900

    cate service#2

--- src/main/java/com/example/deliver/service/CategoryService.java
+++ src/main/java/com/example/deliver/service/CategoryService.java
@@ -1,4 +1,11 @@
 package com.example.deliver.service;
 
+import com.example.deliver.dto.CategoryList;
+import com.example.deliver.model.DefaultRes;
+import com.example.deliver.model.SigninReq;
+import com.example.deliver.model.SigninRes;
+
 public interface CategoryService {
+
+    DefaultRes<CategoryList> category(CategoryList categoryList);
 }
