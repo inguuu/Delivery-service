138855fa36d875e94a6579de0d249390f27e579e
inguuu <dlsrb8949@gmail.com>
2019-08-29 19:33:51 +0900

    menu serviceimp#1

--- src/main/java/com/example/deliver/service/impl/MenuServiceImpl.java
+++ src/main/java/com/example/deliver/service/impl/MenuServiceImpl.java
@@ -1,4 +1,29 @@
 package com.example.deliver.service.impl;
 
-public class MenuServiceImpl {
+import com.example.deliver.dto.CategoryList;
+import com.example.deliver.dto.Menu;
+import com.example.deliver.mapper.MenuMapper;
+import com.example.deliver.model.DefaultRes;
+import com.example.deliver.service.MenuService;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class MenuServiceImpl implements MenuService {
+
+    private final MenuMapper menuMapper;
+
+    public MenuServiceImpl(MenuMapper menuMapper) {
+        this.menuMapper = menuMapper;
+    }
+
+    @Override
+    public DefaultRes<List<Menu>> getMenu(int storeIdx) {
+        List<Menu> menulist = menuMapper.getMenulist(storeIdx)
+        if (menulist.isEmpty()) {
+            return DefaultRes.res(204, "등록된 메뉴가 없습니다.");
+        } else {
+            return DefaultRes.res(HttpStatus.OK.value(), "메뉴 조회 성공.",menulist);
+        }
+    }
 }
