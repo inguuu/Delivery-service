6e515ca2ef4208eddd7202ecfae5ade0af594327
inguuu <dlsrb8949@gmail.com>
2019-08-29 17:24:20 +0900

    cate controller#3

--- src/main/java/com/example/deliver/api/CategoryController.java
+++ src/main/java/com/example/deliver/api/CategoryController.java
@@ -1,4 +1,27 @@
 package com.example.deliver.api;
 
+import com.example.deliver.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping()
+    public ResponseEntity getCategory(@RequestParam int categoryIdx) {
+
+        return new ResponseEntity<>(categoryService.getCategoryList(categoryIdx), HttpStatus.OK);
+
+    }
 }
