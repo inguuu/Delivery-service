bbcd2cf2f1df0f68ffc861d6008c8e24f752bf45
inguuu <dlsrb8949@gmail.com>
2019-08-29 17:07:52 +0900

    cate mapper#2

--- src/main/java/com/example/deliver/mapper/CategoryMapper.java
+++ src/main/java/com/example/deliver/mapper/CategoryMapper.java
@@ -1,4 +1,17 @@
 package com.example.deliver.mapper;
 
+import com.example.deliver.dto.CategoryList;
+import com.example.deliver.dto.User;
+import com.example.deliver.model.SigninReq;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Locale;
+
+@Mapper
 public interface CategoryMapper {
+
+    @Select("SELECT * FROM Category JOIN CategoryList WHERE categoryIdx =#{category.idx} ")
+    CategoryList getCategorylit(@Param("category") CategoryList categorylist);
 }
