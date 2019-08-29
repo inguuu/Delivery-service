dcd5cbe01b27baa07e70bae1c7e3a7d2a786caa5
inguuu <dlsrb8949@gmail.com>
2019-08-29 18:20:13 +0900

    dta#1

--- src/main/java/com/example/deliver/dto/Banner.java
+++ src/main/java/com/example/deliver/dto/Banner.java
@@ -1,4 +1,12 @@
 package com.example.deliver.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Banner {
+    private String bannerImg;
+    private String bannerImg2;
+    private String bannerImg3;
 }
